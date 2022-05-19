2e6527eb00254cf025dd26b1c5630bc30c69c7e8
SONG JI HYUK <jihy1325@gmail.com>
2022-05-19 17:47:16 +0900

    main page 90% finish

--- app/src/main/java/com/risingcamp/manu/layoutactivity/MainPageActivity.kt
+++ app/src/main/java/com/risingcamp/manu/layoutactivity/MainPageActivity.kt
@@ -2,10 +2,18 @@ package com.risingcamp.manu.layoutactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_page)
+
+
+
+
+
+
     }
 }
